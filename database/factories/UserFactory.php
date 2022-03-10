d51111d11c719f34f7f29d841354eea635a40e03
KingofTime <antonio_neto77@hotmail.com.br>
2022-03-10 16:55:20 -0300

    Cria os factories

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -2,6 +2,7 @@
 
 namespace Database\Factories;
 
+use Faker\Provider\pt_BR\Person;
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Support\Str;
 
@@ -17,10 +18,14 @@ class UserFactory extends Factory
      */
     public function definition()
     {
+        $faker_br = new Person($this->faker);
         return [
             'name' => $this->faker->name(),
+            'login' => $this->faker->userName(),
+            'cpf' => $faker_br->cpf(),
             'email' => $this->faker->unique()->safeEmail(),
             'email_verified_at' => now(),
+            'profile_id' => 1,
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
             'remember_token' => Str::random(10),
         ];
