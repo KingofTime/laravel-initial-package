c82dd67082f0ff35c309187028862c1109badfd5
KingofTime <antonio_neto77@hotmail.com.br>
2022-03-12 22:41:15 -0300

    Cria os arquivos de stub

--- routes/api.php
+++ routes/api.php
@@ -20,8 +20,5 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::post('profiles', [ProfileController::class, 'store']);
-Route::get('profiles', [ProfileController::class, 'index']);
-Route::get('profiles/{id}', [ProfileController::class, 'show']);
-Route::put('profiles/{id}', [ProfileController::class, 'update']);
-Route::delete('profiles/{id}', [ProfileController::class, 'destroy']);
+Route::apiResource('profiles', ProfileController::class);
+
